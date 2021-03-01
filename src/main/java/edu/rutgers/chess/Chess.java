ea027095e9071e77fbd83a44e7ae1bd79fdab9d0
Jared Tulayan <jared@jaredtulayan.xyz>
2021-02-28 21:53:16 -0500

    Change project runner

---
+++ src/main/java/edu/rutgers/chess/Chess.java
@@ -0,0 +1,17 @@
+package edu.rutgers.chess;
+
+/**
+ * Main runner class
+ * 
+ * @author Oscar Bartolo
+ * @author Jared Tulayan
+ */
+public class Chess {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Chess().getGreeting());
+    }
+}
