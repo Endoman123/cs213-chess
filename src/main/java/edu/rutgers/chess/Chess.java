cd767efaa58f4831b28d533b0ec2c27da62445fd
Jared Tulayan <jared@jaredtulayan.xyz>
2021-03-18 00:18:23 -0400

    Fix constant evaluation of curKing

--- src/main/java/edu/rutgers/chess/Chess.java
+++ src/main/java/edu/rutgers/chess/Chess.java
@@ -1,6 +1,10 @@
 package edu.rutgers.chess;
 
+import java.util.Arrays;
+
 import edu.rutgers.chess.Board;
+import edu.rutgers.chess.util.Bitboards;
+import edu.rutgers.chess.util.Moves;
 
 /**
  * Main runner class
@@ -10,9 +14,18 @@ import edu.rutgers.chess.Board;
  */
 public class Chess {
     public static void main(String[] args) {
-        Board board = new Board();
+        Board board = new Board(new char[][] {
+           {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+           {'P', 'P', 'P', 'P', ' ', 'P', 'P', 'P'},
+           {' ', ' ', ' ', ' ', 'P', ' ', ' ', ' '},
+           {' ', ' ', ' ', ' ', ' ', ' ', ' ', 'q'},
+           {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+           {' ', ' ', ' ', ' ', 'p', ' ', ' ', ' '},
+           {'p', 'p', 'p', 'p', ' ', 'p', 'p', 'p'},
+           {'r', 'n', 'b', ' ', 'k', 'b', 'n', 'r'},
+       });
 
         System.out.println(board.toString());
-        System.out.println(board.createMemento());
+        System.out.println(Arrays.toString(Moves.gen_pawn_moves(board, 6, 2)));
     }
 }
